ef1c559d9169753e53f8c2e3d97d9d550a6c568c
Bharathi Bandi <bharathi.bandi45@gmail.com>
2024-03-06 15:41:55 +0530

    Initial Code Push

---
+++ src/main/java/com/example/AuthUserController.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+public class AuthUserController {
+
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+
+    }
+
+    @GetMapping("/user_details")
+    public OAuth2User user() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return (OAuth2User) authentication.getPrincipal();
+    }
+}
